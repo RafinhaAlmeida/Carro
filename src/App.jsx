74f2a7b1c0ace7fab5b7d03f0ba29345e13c1ccb
Rafael Rodrigues <s0l1d3r1c3@gmail.com>
2024-03-28 01:17:06 -0300

    segundo commit do app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import iconSedans from "./assets/icon-sedans.svg"
+import iconSuvs from "./assets/icon-suvs.svg"
+import iconLuxury from "./assets/icon-luxury.svg"
+
+
 export default function app(){
   return(
-    <h1>Hello World!</h1>
-  )
+    <main>
+      <section>
+
+        <div>
+          <img src={iconSedans} alt="icone dos carros sedan" />
+          <h2>Sedans</h2>
+          <p>escolha o sedan pelo seu preço acessivel e exelente economia de cobustivel. ideal para dirigir na cidade ou na sua proxima viagem</p>
+          <a href="https://www.webmotors.com.br/catalogo/carroceria/sedan/" target="_blank">ver mais</a>
+        </div>
+
+        <div>
+          <img src={iconSuvs} alt="icone dos carros suvs" />
+          <h2>Suvs</h2>
+          <p>escolha o suvs pelo seu preço acessivel e exelente economia de cobustivel. ideal para dirigir na cidade ou na sua proxima viagem</p>
+          <a href="https://www.webmotors.com.br/suvs" target="_blank">ver mais</a>
+        </div>
+
+        <div>
+          <img src={iconLuxury} alt="icone dos carros Luxury" />
+          <h2>Luxury</h2>
+          <p>escolha o Luxury pelo seu preço acessivel e exelente economia de cobustivel. ideal para dirigir na cidade ou na sua proxima viagem</p>
+          <a href="https://www.carwale.com/new/best-luxury-cars/" target="_blank">ver mais</a>
+        </div>
+
+      </section>
+    </main> 
+ )
 }

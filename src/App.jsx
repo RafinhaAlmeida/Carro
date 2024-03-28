4a4b94711ef4fb9692428c0e92cc9a5822fc47b0
Rafael Rodrigues <s0l1d3r1c3@gmail.com>
2024-03-28 03:01:03 -0300

    segundo e ultimo commit da responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,27 @@ import iconLuxury from "./assets/icon-luxury.svg"
 export default function app(){
   return(
     <main className="min-h-svh bg-offwhitebg py-20 px-6 antialiased lg-flex justift-center items-center">
-      <section className="lg:flex">
+      <section className="lg:flex max-w-[920px]">
 
-        <div className="bg-orange p-12 rounded-t-lg">
+        <div className="bg-orange p-12 max-lg:rounded-t-lg lg:rounded-l-lg">
           <img src={iconSedans} alt="icone dos carros sedan" className="mb-7" />
           <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">Sedans</h2>
-          <p className="font-Lexend mb-7 text-offwhite">escolha o sedan pelo seu preço acessivel e exelente economia de cobustivel. ideal para dirigir na cidade ou na sua proxima viagem</p>
-          <a className="font-Lexend bg-offwhitebg px-10 py-3 w-fit rounded-3xl text-orange" href="https://www.webmotors.com.br/catalogo/carroceria/sedan/" target="_blank">ver mais</a>
+          <p className="font-Lexend mb-7 text-offwhite lg:mb-20">escolha o sedan pelo seu preço acessivel e exelente economia de cobustivel. ideal para dirigir na cidade ou na sua proxima viagem</p>
+          <a className="font-Lexend bg-offwhitebg px-10 py-3 w-fit rounded-3xl text-orange border-2 border-offwhitebg hover:bg-orange hover:text-offwhitebg" href="https://www.webmotors.com.br/catalogo/carroceria/sedan/" target="_blank">ver mais</a>
         </div>
 
         <div className="bg-green p-12"> 
           <img src={iconSuvs} alt="icone dos carros suvs" className="mb-7"/>
           <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">Suvs</h2>
-          <p className="font-Lexend mb-7 text-offwhite">escolha o suvs pelo seu preço acessivel e exelente economia de cobustivel. ideal para dirigir na cidade ou na sua proxima viagem</p>
-          <a className="font-Lexend bg-offwhitebg px-10 py-3 w-fit rounded-3xl text-green" href="https://www.webmotors.com.br/suvs" target="_blank">ver mais</a>
+          <p className="font-Lexend mb-7 text-offwhite lg:mb-20">escolha o suvs pelo seu preço acessivel e exelente economia de cobustivel. ideal para dirigir na cidade ou na sua proxima viagem</p>
+          <a className="font-Lexend bg-offwhitebg px-10 py-3 w-fit rounded-3xl text-green border-2 border-offwhitebg hover:bg-green hover:text-offwhitebg" href="https://www.webmotors.com.br/suvs" target="_blank">ver mais</a>
         </div>
 
-        <div className="bg-dark-green p-12 rounded-b-lg"> 
+        <div className="bg-dark-green p-12 max-lg:rounded-b-lg lg:rounded-r-lg"> 
           <img src={iconLuxury} alt="icone dos carros Luxury" className="mb-7" />
           <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">Luxury</h2>
-          <p className="font-Lexend mb-7 text-offwhite">escolha o Luxury pelo seu preço acessivel e exelente economia de cobustivel. ideal para dirigir na cidade ou na sua proxima viagem</p>
-          <a className="font-Lexend bg-offwhitebg px-10 py-3 w-fit rounded-3xl text-dark-green" href="https://www.carwale.com/new/best-luxury-cars/" target="_blank">ver mais</a>
+          <p className="font-Lexend mb-7 text-offwhite lg:mb-20">escolha o Luxury pelo seu preço acessivel e exelente economia de cobustivel. ideal para dirigir na cidade ou na sua proxima viagem</p>
+          <a className="font-Lexend bg-offwhitebg px-10 py-3 w-fit rounded-3xl text-dark-green border-2 border-offwhitebg hover:bg-dark-green hover:text-offwhitebg" href="https://www.carwale.com/new/best-luxury-cars/" target="_blank">ver mais</a>
         </div>
 
       </section>
